e0eaf14528bbc2f49813895d6bd8f082e7c714ee
le-hachem <personal@hachem.cc>
2025-12-09 21:34:35 +0100

    add: banner + qol

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,9 @@ export const error = color("red", "ansi");
 export const warn = color("yellow", "ansi");
 export const info = color("cyan", "ansi");
 export const reset = "\x1b[0m"; // ANSI reset
+
+// Branding
+export const brandRed = color("#e54c4c", "ansi"); // Bright red
+export const brandDarkRed = color("#9a0000", "ansi"); // Dark red
+export const brand = `${brandRed}Duels${brandDarkRed}+${reset}`;
+export const prompt = `${brandRed}d${brandDarkRed}+${reset}> `;
