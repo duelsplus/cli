c9aaad0b38f8f9d649099ec2c874cd254ce82129
kneesdev <kneesdev@naver.com>
2025-12-10 16:20:03 +0200

    fix: use proper color scheme for brand colors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,7 +6,7 @@ export const info = color("cyan", "ansi");
 export const reset = "\x1b[0m"; // ANSI reset
 
 // Branding
-export const brandRed = color("#e54c4c", "ansi"); // Bright red
-export const brandDarkRed = color("#9a0000", "ansi"); // Dark red
+export const brandRed = color("#ff5555", "ansi"); // Bright red
+export const brandDarkRed = color("#aa0000", "ansi"); // Dark red
 export const brand = `${brandRed}Duels${brandDarkRed}+${reset}`;
 export const prompt = `${brandRed}d${brandDarkRed}+${reset}$ `;
